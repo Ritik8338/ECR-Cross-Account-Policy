6edfb3449cd49bc46191566af64e5391c900e352
Ritik8338 <101251371+Ritik8338@users.noreply.github.com>
2023-07-18 16:13:52 +0530

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,58 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket = "dkhundley-terraform-testsdkjfhsjd"
-    region = "us-east-1"
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bhghigygucket"
 
-## DATA BLOCKS
-## ----------------------------------------------------------------
-
-# Creating the data that will be used by the IAM policy
-data "aws_iam_policy_document" "s3_ls_policy_data" {
-  statement {
-    actions = [
-      "s3:ListAllMyBuckets"
-    ]
-
-    resources = [
-      "arn:aws:s3:::*"
-    ]
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
-}
-
-# Creating the assume role policy data
-data "aws_iam_policy_document" "s3_ls_assume_role_data" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-  }
-}
-
-
-
-## RESOURCE BLOCKS
-## ----------------------------------------------------------------
-
-# Creating the IAM policy using the data block from above
-resource "aws_iam_role_policy" "s3_ls_policy" {
-  name   = "s3_ls_iam_policy"
-  policy = data.aws_iam_policy_document.s3_ls_policy_data.json
-  role   = aws_iam_role.s3_ls_role.id
-}
-
-# Creating the IAM role associated to the resources above
-resource "aws_iam_role" "s3_ls_role" {
-  name               = "s3_ls_iam_role"
-  description        = "This role allows for all S3 buckets to be listed."
-  assume_role_policy = data.aws_iam_policy_document.s3_ls_assume_role_data.json
 }
