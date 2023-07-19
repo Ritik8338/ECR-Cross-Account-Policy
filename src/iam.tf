d7e06a67fa29ddc290ca7ccbe21d89c8e494ebd3
Ritik8338 <101251371+Ritik8338@users.noreply.github.com>
2023-07-19 13:58:26 +0530

    Update iam.tf

--- src/iam.tf
+++ src/iam.tf
@@ -5,7 +5,7 @@
 resource "aws_iam_role" "notebook_iam_role" {
   name = "sagemaker-assume-role"  # Replace with your desired role name
 
-  sm_assume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
