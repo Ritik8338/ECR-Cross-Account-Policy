8b8bef9c30a0469ce8357603ba5de2887e5d0ab5
Ritik8338 <101251371+Ritik8338@users.noreply.github.com>
2023-07-19 11:34:40 +0530

    sphinx issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,8 +2,7 @@
 import os
 import sys
 # Test Need this so sphinx can find lumache.py. Change is .py files are elsewhere than root.
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information
 
@@ -34,12 +33,6 @@ autoapi_dirs = ['../../src']
 #apidoc_excluded_paths = ['tests']
 #apidoc_separate_modules = True
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
 templates_path = ['_templates']
 
 # -- Options for HTML output
@@ -47,36 +40,9 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
 
-html_context = {
-    "display_github": True, # Integrate GitHub
-    "github_repo": "eeholmes/readthedoc-test", # Repo name
-    "github_version": "main", # Version
-    "conf_py_path": "docs/source/", # Path in the checkout to the docs root
-}
-
-# -- Options for HTML output -------------------------------------------------
-
-html_short_title = "topobathy"
-html_show_sourcelink = False
-html_show_sphinx = True
-html_show_copyright = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-repository_url = f"https://github.com/ICESAT-2HackWeek/coastal_topobathy"
-html_context = {
-    "menu_links": [
-        (
-            '<i class="fa fa-github fa-fw"></i> Source Code',
-            repository_url,
-        ),
-        (
-            '<i class="fa fa-book fa-fw"></i> License',
-            f"{repository_url}/blob/main/LICENSE",
-        ),
-    ],
-}
